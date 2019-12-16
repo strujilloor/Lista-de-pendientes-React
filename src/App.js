fddbd05a6c2c2e46b5f418ee09fd968457713be2
Stiven Trujillo <strujillo1293@gmail.com>
2019-12-16 08:55:52 -0500

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Item extends Component {
+  render() {
+    const { item } = this.props
+
+    return (
+      <div>
+        { item.text }
+      </div>
+    )
+  }
 }
 
-export default App;
+class App extends Component {
+  state = {
+    list: []
+  }
+
+  agregar = (e) => {
+    e.preventDefault()
+    const text = e.target[0].value
+    const id = Math.random().toString(16)
+    const pendiente = { text, id }
+    this.setState( state => ({
+      list: [
+        ...state.list,
+        pendiente
+      ]
+    }))
+    e.target[0].value = ''
+  }
+
+  render () {
+
+    return (
+      <div>
+        <h3>shouldComponentUpdate</h3>
+        <form onSubmit={this.agregar}>
+          <input 
+            type="text" 
+            placeholder='Ingresa tu pendiente' 
+          />
+          <button>
+            Agregar
+          </button>
+        </form>
+        <div>
+          { this.state.list.map(item => (
+            <Item 
+              key={ item.id }
+              item={ item }
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
